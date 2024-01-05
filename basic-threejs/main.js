7fb91e770d34ca39eb06a18e7df65070eee6d813
Sky <nebulass@naver.com>
2024-01-06 00:08:38 +0900

    glb 파일 import

--- basic-threejs/main.js
+++ basic-threejs/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -20,16 +21,10 @@ light.lookAt(0, 0, 0);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: "red" });
 const floorGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 역할
-const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
-const capsuleG = new THREE.CapsuleGeometry(1, 2, 20, 30);
-const capsuleM = new THREE.MeshStandardMaterial({ color: "gold" });
-const cylinderG = new THREE.CylinderGeometry(1, 1, 2);
-const cylinderM = new THREE.MeshStandardMaterial({ color: "blue" });
+const floorMaterial = new THREE.MeshBasicMaterial({ color: "gray" });
 
 const mesh = new THREE.Mesh(geometry, material);
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-const capsule = new THREE.Mesh(capsuleG, capsuleM);
-const cylinder = new THREE.Mesh(cylinderG, cylinderM);
 
 mesh.position.y = 0.5;
 mesh.castShadow = true;
@@ -37,21 +32,20 @@ mesh.receiveShadow = true;
 floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true; // 바닥이 도형의 그림자를 받을 수 있게
 floor.castShadow = true;
-capsule.position.y = 2;
-capsule.position.x = -2;
-capsule.castShadow = true;
-capsule.receiveShadow = true;
 
-cylinder.position.set(3, 1, 0);
-cylinder.castShadow = true;
-cylinder.receiveShadow = true;
-
-scene.add(mesh);
+// scene.add(mesh);
 scene.add(floor);
-scene.add(capsule);
-scene.add(cylinder);
-
 scene.add(light);
+
+// dancer import
+const loader = new GLTFLoader();
+loader.load("/dancer.glb", (data) => {
+  const dancer = data.scene;
+  dancer.position.y = 0.8;
+  dancer.scale.set(0.01, 0.01, 0.01);
+  scene.add(dancer);
+});
+
 // scene.add(came)
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);

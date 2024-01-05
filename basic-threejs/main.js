cfa789c4fa0539b7de3dad327e6438bbdb852572
오하늘 <>
2024-01-05 17:54:33 +0900

    castShadow, receviceShacow 등 그림자, capsule 추가

--- basic-threejs/main.js
+++ basic-threejs/main.js
@@ -4,7 +4,7 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.z = 5;
+camera.position.z = 10;
 camera.position.y = 1;
 
 const light = new THREE.DirectionalLight("white", 5); // 태양빛
@@ -20,18 +20,30 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: "red" })
 const floorGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 역할
 const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
+const capsuleG = new THREE.CapsuleGeometry(1, 2, 20, 30);
+const capsuleM = new THREE.MeshStandardMaterial({color: "gold"})
 
 
 
 const mesh = new THREE.Mesh(geometry, material);
 const floor = new THREE.Mesh(floorGeometry, floorMaterial)
-mesh.position.y = 0.5
+const capsule = new THREE.Mesh(capsuleG, capsuleM);
+
+
+mesh.position.y = 0.5;
+mesh.castShadow = true;
+mesh.receiveShadow = true
 floor.rotation.x = -Math.PI / 2
 floor.receiveShadow = true; // 바닥이 도형의 그림자를 받을 수 있게
 floor.castShadow = true;
+capsule.position.y = 2;
+capsule.position.x = -2;
+capsule.castShadow = true;
+capsule.receiveShadow = true;
 
 scene.add(mesh);
 scene.add(floor)
+scene.add(capsule)
 scene.add(light)
 // scene.add(came)
 const renderer = new THREE.WebGLRenderer({antialias: true})

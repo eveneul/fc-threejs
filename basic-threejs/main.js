2730723e1252fb11f9606f32865e9c3a06f4e3b7
오하늘 <>
2024-01-05 17:50:45 +0900

    floor 추가

--- basic-threejs/main.js
+++ basic-threejs/main.js
@@ -5,7 +5,7 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 camera.position.z = 5;
-camera.position.y = 1
+camera.position.y = 1;
 
 const light = new THREE.DirectionalLight("white", 5); // 태양빛
 light.castShadow = true; // 동적 그림자가 드리워짐, 
@@ -17,15 +17,26 @@ light.lookAt(0, 0, 0)
 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({color: "red"})
+const material = new THREE.MeshStandardMaterial({ color: "red" })
+const floorGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 역할
+const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
+
+
 
 const mesh = new THREE.Mesh(geometry, material);
+const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+mesh.position.y = 0.5
+floor.rotation.x = -Math.PI / 2
+floor.receiveShadow = true; // 바닥이 도형의 그림자를 받을 수 있게
+floor.castShadow = true;
 
 scene.add(mesh);
+scene.add(floor)
 scene.add(light)
 // scene.add(came)
 const renderer = new THREE.WebGLRenderer({antialias: true})
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true; // 그림자
 document.body.appendChild(renderer.domElement);
 
 

666f09a0beac28c770f239b72f7745ba037c7fb7
오하늘 <>
2024-01-22 17:10:24 +0900

    webgl shader

--- webgl-shader/main.js
+++ webgl-shader/main.js
@@ -16,20 +16,66 @@ const gl = canvas.getContext("webgl");
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
 
+attribute vec2 position;
 
+void main () {
+  gl_Position = vec4(position, 0.0, 1.0);
+}
 `)
 
 gl.compileShader(vertexShader)
 
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-gl.shaderSource(fragmentShader, ``)
-gl.compileShader(frameElement)
+gl.shaderSource(fragmentShader, `
+
+precision mediump float;
+// 색상과 조명 같은 값은 정확도와 성능을 조절하는 데에 영향을 끼침 // mediump => 중간 정도의 정밀도와 성능 제공, 보편적으로 사용
+
+void main () {
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`)
+gl.compileShader(fragmentShader)
 
 // 버텍스 셰이더랑 프래그먼트 셰이더를 연결
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader)
-gl.attachShader(program, fragmentShader)
+const program = gl.createProgram(); // 프로그램을 만들고
+gl.attachShader(program, vertexShader) // program과 vertexshader를 연결
+gl.attachShader(program, fragmentShader) // program과 fragment shader를 연결
 
-gl.linkProgram(program)
-gl.useProgram(program)
+gl.linkProgram(program) // vertex shader, fragment shader 하나로 합치기
+gl.useProgram(program)  // 하나로 합친 program을 사용하겠다고 명시해 주기
+
+// 정점 데이터를 선언해 주어야 함 (좌표값이 어디에 있는지)
+const vertices = new Float32Array([-1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, -1]);
+// 버텍스 셰이더에 데이터를 넘겨 주기 위해서는 버퍼 데이터로 넘겨 주어야 함
+// buffer = gpu에서 사용할 수 있는 데이터를 저장하는 메모리 영역
+// webgl에서는 vertex data의 위치, 색상, 텍스트 좌표 등의 정보를 사용하는 데에 사용
+
+const vertexBuffer = gl.createBuffer() // buffer 객체를 생성
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);  // 생성한 버퍼를 gl에 연결, array_buffer는 바인딩할 버퍼의 유형, 배열로 전달
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW); // 바인딩된 버퍼를 전송, 첫 번째 인자 => 전송할 버퍼 유형 (어레이) / 두 번째는 실제 전송할 데이터, 세 번째는 gpu에서 데이터를 어떻게 관리할 건지(STATIC_DRAW는 데이터가 자주 변경되지 않고, 그리기 목적으로 주로 사용될 것임을 알림)
+
+// 정점의 위치를 어떻게 계산할지
+const position = gl.getAttribLocation(program, "position") // program에서 특정한 속성의 위치를 찾는 데에 사용, position은 program 내에서 정의된 속성 // 속성의 위치를 나타내는 정수 반환
+
+
+// vertexAttribPointer:: 정점 속성에 대한 데이터 정보를 webGL에게 전달
+
+gl.vertexAttribPointer(
+  position, // 설정할 정점 속성의 위치
+  2, // vertices을 두 개씩 끊어서 읽게
+  gl.FLOAT, // 데이터 요소의 타입, 여기에서는 실수
+  false, // 데이터를 정규화할지에 대한 여부, 이미 데이터가 적절한 형식이면 false,
+  0, // 데이터 간의 간격, 연속적인 데이터라면 0
+  0 // offset 설정, 처음부터 시작할 거라면 0
+)
+
+gl.enableVertexAttribArray(position) // 셰이더에서 이 위치의 정점 데이터를 사용할 준비가 되었다, 렌더링할 때 필수적
+gl.drawArrays( // 데이터(어레이)로 데이터 그릴 때
+  gl.TRIANGLES, // 삼각형을 그리겠다
+  0, // 0번째부터
+  6, // 아이템이 6개가 있다
+)
+
+

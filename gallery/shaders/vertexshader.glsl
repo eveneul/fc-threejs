e65c1a59f4a2dfd9c93f64167a32622f4d813a90
오하늘 <>
2024-01-29 16:49:48 +0900

    vertexshader 마우스 호버 이벤트

--- gallery/shaders/vertexshader.glsl
+++ gallery/shaders/vertexshader.glsl
@@ -1,7 +1,18 @@
 varying vec2 vUv;
+uniform float uTime;
+uniform float uHover;
 
 void main () {
-  gl_Position =  projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  vec2 toCenter = uv - 0.5;
+  float dist = length(toCenter);
+  float dir = dot(toCenter, vec2(1.0, 1.0));
+  float strength = 2.5;
+  float wave = sin(dist * 20.0 - uTime *  10.0);
+
+  vec4 modelPosition =  modelMatrix * vec4(position, 1.0);
+  modelPosition.z += wave * dist * dir * strength * uHover;
+
+  gl_Position =  projectionMatrix * viewMatrix * modelPosition;
 
   vUv = uv;
 }
